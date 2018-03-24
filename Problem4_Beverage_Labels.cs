9467420199c0b7218c367d74dd95e23c4bfc7c05
Maya Raykova <mayaraykova78@gmail.com>
2018-03-24 13:01:38 +0200

    commented Problem 4 and Problem 5

--- Problem4_Beverage_Labels.cs
+++ Problem4_Beverage_Labels.cs
@@ -6,14 +6,15 @@ namespace Pr4_Beverage_Labels
     {
         static void Main(string[] args)
         {
-            string beverageName = Console.ReadLine();
-            double beverageVolume = int.Parse(Console.ReadLine());
-            double energyContentPer100ml = int.Parse(Console.ReadLine());
-            double sugarContentPer100ml = int.Parse(Console.ReadLine());
-
+            string beverageName = Console.ReadLine();// we recive from the Console the beverage Name-> a string
+            
+			double beverageVolume = int.Parse(Console.ReadLine());// we recive from the Console volume -> a integer
+            double energyContentPer100ml = int.Parse(Console.ReadLine()); //we recive from the Console content of energi per 100ml product -> a integer
+            double sugarContentPer100ml = int.Parse(Console.ReadLine());//we recive from the Console content of sugar per 100ml product -> a integer
+				// we calculate...
             double beverageEnergy = beverageVolume * energyContentPer100ml / 100;
             double beverageSugar = beverageVolume * sugarContentPer100ml / 100;
-
+				//TODO
             Console.WriteLine($"{beverageVolume}ml {beverageName}:");
             Console.WriteLine($"{beverageEnergy}kcal, {beverageSugar}g sugars");
         }
--- Problem5_Character_Stats.cs
+++ Problem5_Character_Stats.cs
@@ -5,13 +5,13 @@ namespace P5_Character_Stats
     class Character_Stats
     {
         static void Main(string[] args)
-        {
+        {		//we recive from the Console character Name -> a string
             string characterName = Console.ReadLine();
             int currentHealth = int.Parse(Console.ReadLine());
             int maximumHealth = int.Parse(Console.ReadLine());
             int currentEnergy = int.Parse(Console.ReadLine());
             int maximumEnergy = int.Parse(Console.ReadLine());
-
+				//TODO
             Console.WriteLine($"Name: {characterName}");
             Console.WriteLine("Health: |" + new string('|', currentHealth) + new string('.', maximumHealth - currentHealth) + '|');
             Console.WriteLine("Energy: |" + new string('|', currentEnergy) + new string('.', maximumEnergy - currentEnergy) + '|');
