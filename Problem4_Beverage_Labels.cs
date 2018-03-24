b2ddabcf6bb1f2dd96e66862f6da9155f66f86e6
VeselinStAleksandrov <vn.aleksandrov@abv.bg>
2018-03-24 11:12:18 +0200

    Solutions of Problems 4 and 5 added

---
+++ Problem4_Beverage_Labels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Pr4_Beverage_Labels
+{
+    class Beverage_Labels
+    {
+        static void Main(string[] args)
+        {
+            string beverageName = Console.ReadLine();
+            double beverageVolume = int.Parse(Console.ReadLine());
+            double energyContentPer100ml = int.Parse(Console.ReadLine());
+            double sugarContentPer100ml = int.Parse(Console.ReadLine());
+
+            double beverageEnergy = beverageVolume * energyContentPer100ml / 100;
+            double beverageSugar = beverageVolume * sugarContentPer100ml / 100;
+
+            Console.WriteLine($"{beverageVolume}ml {beverageName}:");
+            Console.WriteLine($"{beverageEnergy}kcal, {beverageSugar}g sugars");
+        }
+    }
+}
